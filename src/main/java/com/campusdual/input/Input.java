f54f5168fd5300feaba1c3afb0cce8f39be69edd
Daniel <dani.v2509@outlook.com>
2024-10-10 16:32:23 +0200

    Switch Statement

---
+++ src/main/java/com/campusdual/input/Input.java
@@ -0,0 +1,24 @@
+package com.campusdual.input;
+
+import com.campusdual.Utils;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void useScannerToGetName() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Good morning, " + name + "!");
+    }
+
+    public static void useUtilsToGetName() {
+        String name = Utils.string("Write your name: ");
+        System.out.println("Hi " + name);
+    }
+
+    public static void main(String[] args) {
+       // useScannerToGetName();
+        useUtilsToGetName();
+    }
+}
