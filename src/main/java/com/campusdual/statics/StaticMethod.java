ae7ef846ec18635a0a24ab6927b4fd4fce6014aa
Daniel <dani.v2509@outlook.com>
2024-10-16 09:45:16 +0200

    Métodos estáticos

---
+++ src/main/java/com/campusdual/statics/StaticMethod.java
@@ -0,0 +1,28 @@
+package com.campusdual.statics;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class StaticMethod {
+
+    // String, ya que pretendemos devolver un mensaje
+    public static String countDownToYear(String name, int year){
+        Date today = new Date();
+        System.out.println(today);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, Calendar.JANUARY, 1, 0, 0, 0);
+        Date targetDate = calendar.getTime();
+        long miliseconds = targetDate.getTime() - today.getTime();
+        long days = miliseconds / (1000 * 60 * 60 * 24);
+
+        if (days > 0){
+            return "hi " + name + "!, countdown to " + year + ": " + days + " days.";
+        } else {
+            return "hi " + name + "!, countdown to " + year + ": " + Math.abs(days) + " days.";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StaticMethod.countDownToYear("Daniel", 2025));
+    }
+}
