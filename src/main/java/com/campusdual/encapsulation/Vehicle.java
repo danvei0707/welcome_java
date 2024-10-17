5c181dc0a576284d08cd819238dd8dfaf588bc0f
Daniel <dani.v2509@outlook.com>
2024-10-17 09:51:57 +0200

    Métodos de Car

--- src/main/java/com/campusdual/encapsulation/Vehicle.java
+++ src/main/java/com/campusdual/encapsulation/Vehicle.java
@@ -2,13 +2,13 @@ package com.campusdual.encapsulation;
 
 public class Vehicle {
     // Definir atributos
-    public String brand;
-    public String licensePlate;
-    public String model;
+    private String brand;
+    private String licensePlate;
+    private String model;
     public boolean available;
-    public static final int LEGAL_MAX_SPEED = 120;
+    private static final int LEGAL_MAX_SPEED = 120;
 
-    public Vehicle(String brand, String licensePlate, String model) {
+    public Vehicle(String licensePlate, String brand, String model) {
         this.brand = brand;
         this.licensePlate = licensePlate;
         this.model = model;
@@ -45,5 +45,40 @@ public class Vehicle {
 
     public void setAvailable(boolean available) {
         this.available = available;
+    }
+
+    public void rentACar(){
+        if (this.available){
+            this.available = false;
+            System.out.println("The vehicle with licence plate " + this.licensePlate + " has been rented");
+        }
+        else {
+            System.out.println("The vehicle with licence plate " + this.licensePlate + " is not available");
+        }
+    }
+
+    public void returnACar(){
+        if (!this.available){
+            this.available = true;
+            System.out.println("The vehicle with licence plate " + this.licensePlate + " has been returned");
+        } else {
+            System.out.println("The vehicle with licence plate " + this.licensePlate + " is already available");
+        }
+    }
+
+    public String getDetails(){
+        return this.brand + " " + this.model + "\n\t License plate: " + this.licensePlate + "\n\t Available:" + (this.available?"Yes":"No");
+    }
+
+    public static void main(String[] args){
+        Vehicle v = new Vehicle("94311FYZ", "Mazda", "5");
+        System.out.println(v.getDetails());
+        v.rentACar();
+        System.out.println(v.getDetails());
+        v.rentACar();
+        v.returnACar();
+        System.out.println(v.getDetails());
+        v.returnACar();
+
     }
 }
